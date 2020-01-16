108426cb9ec79b2cb734f6ed2377ca1b788f7e72
nalcala <alcalan@fellows.iarc.fr>
2020-01-16 14:31:02 +0100

    add scripts for genotyping

---
+++ bin/prep_vcf_bed.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for v in `ls *.vcf`; do cat $v | vcf2bed > "$v.bed"; done
+beds=''
+for b in `ls *.bed`; do beds=$beds' '$b; done
+bedops -m $beds > regionsI.bed
+awk '{print $1"\t"($2)"\t"($3)}' regionsI.bed | sed -r 's/-[0-9]?[0-9]/1/g' | bedops -m /dev/stdin > regions.bed
+#bgzip regions.bed
+#tabix -p bed regions.bed.gz
